bbb0bb186406557daaa398f73a334240bcced61e
Abhishek Goyal <goyal.abhishek.it@gmail.com>
2020-02-03 01:20:14 -0500

    Added redirection API

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const request = require('request');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+    const q = req.query.q;
+
+    request({
+      url: q,
+      method: req.method,
+      body: req.body,
+      headers: {
+        'User-Agent': req.header('User-Agent'),
+      }
+    }, function (err, response, body) {
+      res.send(body);
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
